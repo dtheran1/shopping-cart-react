2cf0ba86f11e1af527665ed7f2b91503fa2bc784
Dtheran <dtheran1@gmail.com>
2023-02-21 17:31:58 -0500

    implement useReducer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,10 @@
-import { useCart } from '../hooks/useCart'
 import('./footer.css')
 
 export function Footer () {
-  const { cart } = useCart()
   return (
     <footer className='footer'>
-      {
-        /*
-        <h4>Prueba técnica de React ⚛️ － <span>@dtheran1</span></h4>
-        <h5>Shopping Cart con useContext & useReducer</h5>
-        */
-      }
-      {JSON.stringify(cart, null, 2)}
-
+      <h4>Prueba técnica de React ⚛️ － <span>@dtheran1</span></h4>
+      <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
 
   )
---
+++ src/reducers/cart.js
@@ -0,0 +1,35 @@
+export const cartInitialState = []
+export const cartReducer = (state, action) => {
+  const { type: actionType, payload: actionPayload } = action
+  switch (actionType) {
+    case 'ADD_TO_CART': {
+      const { id } = actionPayload
+      const productInCartIndex = state.findIndex(item => item.id === id)
+
+      if (productInCartIndex >= 0) {
+      // Una forma seria usando el structureClone que hace una copia profunda de obj o arr, para arrays pequenos sirve bien
+        const newState = structuredClone(state)
+        newState[productInCartIndex].quantity += 1
+        return newState
+      }
+
+      return [
+        ...state,
+        {
+          ...actionPayload,
+          quantity: 1
+        }
+      ]
+    }
+
+    case 'REMOVE_FROM_CART': {
+      const { id } = actionPayload
+      return state.filter(item => item.id !== id)
+    }
+
+    case 'CLEAR_CART': {
+      return cartInitialState
+    }
+  }
+  return state
+}
